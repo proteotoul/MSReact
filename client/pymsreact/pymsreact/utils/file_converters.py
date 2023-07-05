11931e8cd6ebbc2e39d239d30872bdf669249390
Zoltan Udvardy <zoltan.udvardy.ipbs@gmail.com>
2023-07-05 11:13:53 +0200

    30-prm-improvements - FEAT -Client:   * Use later java version in proline data extractor.   * Transmit messages from software ran in process to the logging module.   * Check in jpt target list.   * Add first version of searchgui wrappers. - Server:   * Add HttpFileServer.cs.

--- client/pymsreact/pymsreact/utils/file_converters.py
+++ client/pymsreact/pymsreact/utils/file_converters.py
@@ -2,7 +2,7 @@ import logging
 import time
 import os
 import subprocess
-from subprocess import Popen, CREATE_NEW_CONSOLE
+from subprocess import Popen, PIPE, STDOUT, CREATE_NEW_CONSOLE
 from contextlib import contextmanager
 from .temp_environ import temp_environ
 
@@ -20,7 +20,7 @@ class FileConverters:
     NO_ERROR = 0
 
     def __init__(self):
-        pass  
+        self.logger = logging.getLogger(__name__) 
 
     def thermo_to_mzdb(self, args):
         """Python wrapper for raw2mzdb converter."""
@@ -68,17 +68,28 @@ class FileConverters:
         
     def _run_in_process(self, converter, args):
         msg = converter + args
-        self.proc = Popen(msg, creationflags=CREATE_NEW_CONSOLE)
+        self.proc = Popen(msg,
+                          stdout=PIPE, 
+                          stderr=STDOUT)
+        with self.proc.stdout:
+            self._log_subprocess_output(self.proc.stdout)
         return self.proc.wait()
         
+    def _log_subprocess_output(self, pipe):
+        for line in iter(pipe.readline, b''): # b'\n'-separated lines
+            self.logger.info(line.decode("utf-8"))
+        
 if __name__ == "__main__":
+
+    FORMAT = '%(message)s'
+    logging.basicConfig(format=FORMAT, level=20)
     file_converters = FileConverters()
     args_thermo_to_mzdb = \
-        {"-i" : "D:\\analysis\\Exploris\\2023_05_09\\OXZUD230509_03.raw",
-         "-o" : "OXZUD230509_03.raw.mzDB"}
+        {"-i" : "D:\\data\\OFCCF2300322_10.raw",
+         "-o" : "OFCCF2300322_10.raw.mzDB"}
     file_converters.thermo_to_mzdb(args_thermo_to_mzdb)
     args_mzdb_to_mgf = \
-        {"-i" : "OXZUD230509_03.raw.mzDB",
-         "-o" : "OXZUD230509_03.raw.mzDB.mgf"}
+        {"-i" : "OFCCF2300322_10.raw.mzDB",
+         "-o" : "OFCCF2300322_10.raw.mzDB.mgf"}
     file_converters.mzdb_to_mgf(args_mzdb_to_mgf)
     
